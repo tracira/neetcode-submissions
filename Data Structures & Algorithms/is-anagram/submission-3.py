eec2125b16a265afeeafe8daae04d292cba4339f
Guoyueyang Cheng <140793550+tracira@users.noreply.github.com>
2026-08-20 23:28:16 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic_s, dic_t = {}, {}
+        for c in s:
+            dic_s[c] = dic_s.get(c, 0) + 1
+        for c in t:
+            dic_t[c] = dic_t.get(c, 0) + 1
+        
+        return dic_s == dic_t
+
+        # T : O(n1+n2)
+        # S : O(1) Because total amount is limited
